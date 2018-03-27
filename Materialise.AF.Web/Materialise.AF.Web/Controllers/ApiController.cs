caf20f77f2ec4ea3909f4d7e06b536bf6f9d2301
MATONE\lesnikovskiy <Ruslan.Lesnikovskiy@materialise.kiev.ua>
2018-03-27 15:56:04 +0300

    first commit

---
+++ Materialise.AF.Web/Materialise.AF.Web/Controllers/ApiController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Materialise.AF.Web.Controllers
+{
+	public class ApiController : Controller
+	{
+		// GET
+		public IActionResult Index()
+		{
+			return
+			View();
+		}
+	}
+}
