88e0dcfe60f17c8fbb1bbaed626d8e3b6bebef4a
Anastasiia Trepyton <anastasiia.trepyton@materialise.kiev.ua>
2018-04-12 14:36:29 +0300

    add authorization headers for requests

--- Materialise.AF.Web/Materialise.AF.Web/src/app/services/progress.service.ts
+++ Materialise.AF.Web/Materialise.AF.Web/src/app/services/progress.service.ts
@@ -1,32 +1,30 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { UserItem, ItemProgress } from '../models/item';
+import { AuthService } from './auth.service';
 
 import 'rxjs/add/operator/catch';
 import { environment } from '../../environments/environment';
 
-const httpOptions = {
-    headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-    })
-};
 
 @Injectable()
 export class ProgressService {
 
     baseUrl: string;
 
-    constructor(private httpClient: HttpClient) {
+    constructor(private httpClient: HttpClient, private authService: AuthService) {
     }
 
     public getProgressByUser(id: number): Observable<ItemProgress> {
-        return this.httpClient.get<ItemProgress>(`${environment.apiEndpoint}/user/${id}`).map(data => {
+        const httpOptions = this.authService.getAuthorizationHeaders();
+        return this.httpClient.get<ItemProgress>(`${environment.apiEndpoint}/user/${id}`, httpOptions).map(data => {
             return data;
         });
     }
 
     public addToProgress(userItem: UserItem): Observable<UserItem> {
+        const httpOptions = this.authService.getAuthorizationHeaders();
         return this.httpClient.post<UserItem>(`${environment.apiEndpoint}/markers`, userItem, httpOptions);
     }
 }
--- Materialise.AF.Web/Materialise.AF.Web/src/app/services/user.service.ts
+++ Materialise.AF.Web/Materialise.AF.Web/src/app/services/user.service.ts
@@ -3,13 +3,13 @@ import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/map';
 
 import { environment } from '../../environments/environment';
-
 import { User } from '../models/user';
+import { AuthService } from './auth.service';
 
 @Injectable()
 export class UserService {
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient, private authService: AuthService) { }
 
     private apiPath = `${environment.apiEndpoint}/user`;
 
@@ -23,8 +23,8 @@ export class UserService {
 
     create(user: User) {
         const userData = { firstname: user.firstName, lastName: user.lastName, email: user.email };
-
-        return this.http.post<any>(this.apiPath, userData)
+        const httpOptions = this.authService.getAuthorizationHeaders();
+        return this.http.post<any>(this.apiPath, userData, httpOptions)
             .map(data => {
                 if (data && data.token) {
                     localStorage.setItem('token', data.token);
